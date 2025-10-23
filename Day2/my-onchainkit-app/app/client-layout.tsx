fcd1fd3958cecdb66f8f9f55828b47469287d20e
Deepakkumar2206 <deepakjkumar622@gmail.com>
2025-10-23 15:23:11 +0000

    Add Day 2 - Frontend with OnchainKit

---
+++ Day2/my-onchainkit-app/app/client-layout.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { RootProvider } from "./rootProvider";
+
+export default function ClientLayout({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return <RootProvider>{children}</RootProvider>;
+}
